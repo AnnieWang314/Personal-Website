35530e1af0f5a9819cf0c16056166f5e6c83ebd0
Annie Wang <anniewang2023@gmail.com>
2023-07-03 03:17:17 -0400

    fixed home, about, darkmode toggle

--- style.js
+++ style.js
@@ -1,15 +1,15 @@
 /* to make pages */
-const homeDiv = document.getElementById("home-page")
-const aboutDiv = document.getElementById("about-page")
-const projectsDiv = document.getElementById("projects-page")
-const funsiesDiv = document.getElementById("funsies-page")
-const homeLinkDiv = document.getElementById("header-logo")
-const aboutLinkDiv = document.getElementById("header-about")
-const projectsLinkDiv = document.getElementById("header-projects")
-const funsiesLinkDiv = document.getElementById("footer-funsies")
+const homeDiv = document.getElementsByClassName("home-page")[0];
+const aboutDiv = document.getElementsByClassName("about-page")[0];
+const projectsDiv = document.getElementsByClassName("projects-page")[0];
+const funsiesDiv = document.getElementsByClassName("funsies-page")[0];
+const homeLinkDiv = document.getElementById("header-logo");
+const aboutLinkDiv = document.getElementById("header-about");
+const projectsLinkDiv = document.getElementById("header-projects");
+const funsiesLinkDiv = document.getElementById("footer-funsies");
 
 homeLinkDiv.onclick = (_event) => {
-    homeDiv.style.display = "block";
+    homeDiv.style.display = "flex";
     aboutDiv.style.display = "none";
     projectsDiv.style.display = "none";
     funsiesDiv.style.display = "none";
@@ -17,7 +17,7 @@ homeLinkDiv.onclick = (_event) => {
 
 aboutLinkDiv.onclick = (_event) => {
     homeDiv.style.display = "none";
-    aboutDiv.style.display = "block";
+    aboutDiv.style.display = "flex";
     projectsDiv.style.display = "none";
     funsiesDiv.style.display = "none";
 };
@@ -25,7 +25,7 @@ aboutLinkDiv.onclick = (_event) => {
 projectsLinkDiv.onclick = (_event) => {
     homeDiv.style.display = "none";
     aboutDiv.style.display = "none";
-    projectsDiv.style.display = "block";
+    projectsDiv.style.display = "flex";
     funsiesDiv.style.display = "none";
 };
 
@@ -38,12 +38,14 @@ funsiesLinkDiv.onclick = (_event) => {
 
 
 /* dark mode stuff */
-const checkbox = document.getElementById("darkmode-check")
-const homebackground = document.getElementById("home-name")
-const logo = document.getElementById("header-logo")
-const giticon = document.getElementById("git-icon")
-const mailicon = document.getElementById("mail-icon")
-const linkedinicon = document.getElementById("linkedin-icon")
+const checkbox = document.getElementById("darkmode-check");
+const homebackground = document.getElementsByClassName("home-name")[0];
+const logo = document.getElementById("header-logo");
+const giticon = document.getElementById("git-icon");
+const mailicon = document.getElementById("mail-icon");
+const linkedinicon = document.getElementById("linkedin-icon");
+const footerline = document.getElementsByClassName("footer")[0];
+const toggleback = document.getElementsByClassName("switch")[0];
 
 checkbox.addEventListener("change",(event) => {
     if (event.currentTarget.checked) {
@@ -51,18 +53,22 @@ checkbox.addEventListener("change",(event) => {
         homebackground.classList.toggle("dark-mode");
         logo.src="dark-logo.png";
         logo.alt="logo";
-        giticon.style="color: #d1eae7";
-        mailicon.style="color: #d1eae7";
-        linkedinicon.style="color: #d1eae7";
+        giticon.style.color="#d1eae7";
+        mailicon.style.color="#d1eae7";
+        linkedinicon.style.color="#d1eae7";
+        footerline.classList.toggle("dark-mode");
+        toggleback.classList.toggle("dark-mode");
     }
     else {
         document.body.classList.toggle("dark-mode");
         homebackground.classList.toggle("dark-mode");
         logo.src="logo.png";
         logo.alt="logo";
-        giticon.style="color: black";
-        mailicon.style="color: black";
-        linkedinicon.style="color: black";
+        giticon.style.color="black";
+        mailicon.style.color="black";
+        linkedinicon.style.color="black";
+        footerline.classList.toggle("dark-mode");
+        toggleback.classList.toggle("dark-mode");
     }
 })
 
